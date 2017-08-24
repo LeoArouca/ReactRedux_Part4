82f944435285276f88d0992da713f3fe247e2e42
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-24 21:26:59 +0800

    Makes posts fetch work

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,42 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 // Connect the action creator in here
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component{
+
+  componentDidMount(){
+    // go fetch data
+    this.props.fetchPosts();
+  }
+
+  renderPosts(){
+    // Good method to return multiple items
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render(){
+    console.log(this.props.posts);
     return(
-      <div> Posts Index</div>
+      <div>
+        <h3>Posts</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
+      </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
 }
 
 // Passes action creator in, instead of the mapDispatch, it is identical
@@ -16,4 +44,5 @@ class PostsIndex extends Component{
 // export default connect(null, { fetchPosts: fetchPosts} )(PostsIndex);
 
 // ES6
-export default connect(null, { fetchPosts } )(PostsIndex);
+// I missed mapStateToProps here (it was null), nothing would come back from the API request
+export default connect(mapStateToProps, { fetchPosts } )(PostsIndex);
