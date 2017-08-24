26d7816a60cb6e1233552601f77354b0a4a0070e
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-24 21:40:02 +0800

    Items 1 and 2

--- src/index.js
+++ src/index.js
@@ -2,21 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // promise for the async calls
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+// This loads both components
+
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <BrowserRouter>
+//       <div>
+//         <Route path='/' component={PostsIndex} />
+//         <Route path='/posts/new' component={PostsNew} />
+//       </div>
+//     </BrowserRouter>
+//   </Provider>
+//   , document.querySelector('.container'));
+
+
+// When adding Switch to fix this issue above, remember to put the most specific routes at the top and generic
+// at the bototm
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
