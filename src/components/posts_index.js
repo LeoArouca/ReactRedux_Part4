aa17dc441f0f07b30b52697aa3749d585fac6f4f
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-24 21:44:23 +0800

    Makes link add button work in react only

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // Connect the action creator in here
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component{
 
@@ -26,6 +27,11 @@ class PostsIndex extends Component{
     console.log(this.props.posts);
     return(
       <div>
+        <div className='text-xs-right'>
+          <Link className='btn btn-primary' to='/posts/new'>
+            Add a post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className='list-group'>
           {this.renderPosts()}
