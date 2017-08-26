0c5035acc555bf86ce53a5c45bc38833c22b0139
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-26 15:09:05 +0800

    Adds redux form and connects it to the app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+// Import form
+// import this as this alias formReducer
+// Redux form connected
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
