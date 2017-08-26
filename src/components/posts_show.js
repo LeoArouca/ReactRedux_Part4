00c0f08108ce26853014d1d62f2ff35b1e0c3cc6
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-26 19:11:54 +0800

    Wire up redux

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+
+  componentDidMount(){
+    // Provided directly to us by react router
+    const postId = this.props.match.params.id;
+    console.log(postId);
+
+    this.props.fetchPost(postId);
+  }
+
   render(){
+    // this would work but not reusable and ideal
+    // THIS component only cares about one post not ALL
+    // const postId = this.props.match.params.id;
+    // posts[postId];
+
+    // this.props = ownProps;
+
     return (
       <div>Show</div>
     );
   }
 }
 
-export default PostsShow;
+// Only cares about posts (no need to pass entire state)
+// Second argument is the props object that is going to the component above (when its rendered)
+function mapStateToProps({ posts }, ownProps){
+  // for option 1
+  // return { posts };
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchPost })(PostsShow);
